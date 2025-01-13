0067ab147b13d5c4d9f9e0b685af29881b55028a
Sravyareddy04 <162128264+Sravyareddy04@users.noreply.github.com>
2025-01-13 22:47:19 +0530

    Update script.js

--- resume_generator/script.js
+++ resume_generator/script.js
@@ -1,13 +1,9 @@
-//alert("loding");
+// Add New Work Experience Field
 function addNewWEField() {
-    //    console.log("Adding new field")
-
     let newNode = document.createElement("textarea");
-    newNode.classList.add("form-control");
-    newNode.classList.add("weField");
-    newNode.classList.add("mt-2");
+    newNode.classList.add("form-control", "weField", "mt-2");
     newNode.setAttribute("rows", 3);
-    newNode.setAttribute("placeholder","Enter here");
+    newNode.setAttribute("placeholder", "Enter here");
 
     let weOb = document.getElementById("we");
     let weAddButtonOb = document.getElementById("weAddButton");
@@ -15,77 +11,55 @@ function addNewWEField() {
     weOb.insertBefore(newNode, weAddButtonOb);
 }
 
+// Add New Academic Qualification Field
 function addNewAQField() {
     let newNode = document.createElement("textarea");
-    newNode.classList.add("form-control");
-    newNode.classList.add("eqField");
-    newNode.classList.add("mt-2");
+    newNode.classList.add("form-control", "eqField", "mt-2");
     newNode.setAttribute("rows", 3);
-    newNode.setAttribute("placeholder","Enter here");
- 
+    newNode.setAttribute("placeholder", "Enter here");
+
     let aqOb = document.getElementById("aq");
     let aqAddButtonOb = document.getElementById("aqAddButton");
- 
+
     aqOb.insertBefore(newNode, aqAddButtonOb);
 }
 
+// Generate CV
+function generateCV() {
+    let nameField = document.getElementById("namefield").value;
+    document.getElementById("nameT1").innerHTML = nameField;
+    document.getElementById("nameT2").innerHTML = nameField;
 
-//generating cv
+    document.getElementById("contactT").innerHTML = document.getElementById("contactfield").value;
+    document.getElementById("addressT").innerHTML = document.getElementById("addressfield").value;
 
-function generateCV(){
-   // console.log("generating CV")
+    document.getElementById("fbT").innerHTML = document.getElementById("fbfield").value;
+    document.getElementById("instaT").innerHTML = document.getElementById("instafield").value;
+    document.getElementById("LinkedT").innerHTML = document.getElementById("linkedfield").value;
 
-    let nameFeild = document.getElementById("nameField").value;
+    document.getElementById("objectiveT").innerHTML = document.getElementById("ObjectiveField").value;
 
-    let nameT1 = document.getElementById("nameT1")
-
-    nameT1.innerHTML = nameField;
-    //direct
-
-    document.getElementById("nameT2").innerHTML = nameField
-
-    //contact
-    document.getElementById("contactT").innerHTML=document.getElementById("contactField").value;
-
-    //address
-    document.getElementById("addressT").innerHTML=document.getElementById("addressField").value;
-    document.getElementById("fbT").innerHTML=document.getElementById("addressField").value;
-    document.getElementById("instaT").innerHTML=document.getElementById("addressField").value;
-    document.getElementById("LinkedT").innerHTML=document.getElementById("addressField").value;
-
-    //objective
-    document.getElementById("objectiveT").innerHTML=document.getElementById("obectiveField").value;
-
-    //we
-    let wes = document.getElementsByClassName("weField")
-
-    let str="";
-
-    for(let e of wes)
-    {
-        str=str+'<li> ${e.value} </li>';
+    // Work Experience
+    let wes = document.getElementsByClassName("weField");
+    let weStr = "";
+    for (let e of wes) {
+        weStr += `<li>${e.value}</li>`;
     }
+    document.getElementById("weT").innerHTML = weStr;
 
-    document.getElementById("weT").innerHTML = str;
-
-    //aq
-    
-    let aqs=document.getElementsByClassName("eqField")
-
-    let str1="";
-
-    for(let e of aqs)
-    {
-        str1+='<li> ${e.value} </li>';
+    // Academic Qualification
+    let aqs = document.getElementsByClassName("eqField");
+    let aqStr = "";
+    for (let e of aqs) {
+        aqStr += `<li>${e.value}</li>`;
     }
+    document.getElementById("aqT").innerHTML = aqStr;
 
-    document.getElementById("aqT").innerHTML=str1;
-
-    document.getElementById("cv-form").style.display="none"
-    document.getElementById("cv-template").style.display="block";
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
 }
 
-//print cv
-function printCV(){
+// Print CV
+function printCV() {
     window.print();
 }
